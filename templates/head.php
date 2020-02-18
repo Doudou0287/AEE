d9e1e6943131bed9c0322b1562c28f2cb7e42e58
s.alkebsi <s.alkebsi@>
2020-02-18 10:01:23 +0100

    add php structure

---
+++ templates/head.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>AEE</title>
+  
+    
+    <meta charset="UTF-8">
+    <meta name="description" content="Association Étudiant-es Étranger-es">
+    <meta name="keywords" content="Commercials,  événements">
+    <meta name="author" content="Sundus">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"></head>
+    <body>
+
