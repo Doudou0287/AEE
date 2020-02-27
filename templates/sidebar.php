1880a42431bb957f53554168518b22eb67266760
s.alkebsi <s.alkebsi@>
2020-02-27 18:16:52 +0100

    ajouter du code html au fichier sidebar.php

---
+++ templates/sidebar.php
@@ -0,0 +1,26 @@
+
+<div class="container-fluid">
+    <div class="col-md-3">
+
+        <div id="sidebar">
+            <div class="container-fluid tmargin">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Search..." />
+                    <span class="input-group-btn">
+                <button class="btn btn-default"><span class="glyphicon glyphicon-search"></span></button>
+                    </span>
+                </div>
+            </div>
+
+            <ul class="nav navbar-nav side-bar">
+                <li class="side-bar tmargin"><a href="#"><span class="glyphicon glyphicon-education">&nbsp;</span>introduction de l’association.</a></li>
+                <li class="side-bar"><a href="#"><span class="glyphicon glyphicon-calendar">&nbsp;</span> évènement : réservation.</a></li>
+                <li class="side-bar"><a href="#"><span class="glyphicon glyphicon-shopping-cart">&nbsp;</span> e-commerce : vente.</a></li>
+                <li class="side-bar"><a href="#"><span class="glyphicon glyphicon-comment">&nbsp;</span>formulaire</a></li>
+
+
+            </ul>
+        </div>
+    </div>
+
+</div>
