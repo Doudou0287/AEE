4271cb417302b4c90209fb65dfaa85e8baf8c1e5
s.alkebsi <s.alkebsi@>
2020-03-03 08:21:37 +0100

    edit index.php et ajouter footer.php

--- index.php
+++ index.php
@@ -1,16 +1,10 @@
 <?php
 
-require_once("templates/sidebar.php");
+
 require_once("templates/head.php");
 require_once("templates/header.php");
-require_once(".php/ancien_events.php");
-require_once(".php/anglais.php");
-require_once(".php/arabe.php");
-require_once(".php/association.php");
-require_once(".php/formulaire.php");
-require_once(".php/francais.php");
-require_once(".php/menu.php");
-require_once(".php/reseaux_sociaux.php");
+require_once("templates/sidebar.php");
+require_once("template/footer.php");
 
 
 
