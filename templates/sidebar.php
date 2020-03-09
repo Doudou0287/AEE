868ec32aba8bc6ac6bf28b503879b3c310c64848
s.alkebsi <s.alkebsi@>
2020-03-09 15:48:28 +0100

    change sidebar

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -1,31 +1,32 @@
 
-<div class="container-fluid">
-    <div class="col-md-3">
+<div id="sidebar">
 
-        <button class="sidebar-toggler" type="button" data-toggle="collapse" data-target="#sidebarNav" aria-controls="sidebarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <i class="fa fa-bars"></i> 
-        </button>
-        <div class="collapse sidebar-collapse" id="sidebarNav">
-        <div id="sidebar">
-            <div class="container-fluid tmargin">
-                <div class="input-group">
-                    <input type="text" class="form-control" placeholder="Search..." />
-                    <span class="input-group-btn">
-                <button class="btn btn-default"><span class="glyphicon glyphicon-search"></span></button>
-                    </span>
-                </div>
-            </div>
-
-            <ul class="nav sidebar-nav side-bar">
-                <li class="side-bar tmargin"><a href="#"><span class="glyphicon glyphicon-education">&nbsp;</span>introduction de l’association.</a></li>
-                <li class="side-bar"><a href="#"><span class="glyphicon glyphicon-calendar">&nbsp;</span> évènement : réservation.</a></li>
-                <li class="side-bar"><a href="#"><span class="glyphicon glyphicon-shopping-cart">&nbsp;</span> e-commerce : vente.</a></li>
-                <li class="side-bar"><a href="#"><span class="glyphicon glyphicon-comment">&nbsp;</span>formulaire</a></li>
-
-
-            </ul>
-        </div>
+    <div class="toggle-btn" onclick="toggleSidebar()">
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
+    <div class="container-fluid tmargin">
+        <div class="input-group">
+            <input type="text" class="form-control" placeholder="Search..." />
+            <span class="input-group-btn">
+        <button class="btn btn-default"><span class="glyphicon glyphicon-search"></span></button>
+            </span>
         </div>
     </div>
-
+    <ul>
+        <li><a href="assets/.php/association.php"><span class="glyphicon glyphicon-education">&nbsp;</span>introduction de l’association.</a></li>
+        <li><a href="assets/.php/evenement.php"><span class="glyphicon glyphicon-calendar">&nbsp;</span> évènement : réservation.</a></li>
+        <li><a href="assets/.php/commerce.php"><span class="glyphicon glyphicon-shopping-cart">&nbsp;</span> e-commerce : vente.</a></li>
+        <li><a href="assets/.php/formulaire.php"><span class="glyphicon glyphicon-comment">&nbsp;</span>formulaire</a></li>
+        <!-- li class="side-bar"><a href="assets/.php/evenement.php"><span class="glyphicon glyphicon-calendar">&nbsp;</span> évènement : réservation.</a></li-->
+    </ul>
 </div>
+
+
+<script>
+    function toggleSidebar() {
+    document.getElementById("sidebar").classList.toggle('active');
+    }
+
+</script>
