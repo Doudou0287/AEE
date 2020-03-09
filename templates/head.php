52a6a1069d6f2582f2656c551b999366b15d2bb5
s.alkebsi <s.alkebsi@>
2020-03-09 15:45:18 +0100

    change sidebar and header

--- templates/head.php
+++ templates/head.php
@@ -9,7 +9,7 @@
     <meta name="author" content="Sundus">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
  
-
+    <link rel="stylesheet" href="assets/.css/style.css"> <!--linking  style --->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> 
     <link rel="stylesheet" href=https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css> 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
--- templates/header.php
+++ templates/header.php
@@ -1,50 +1,66 @@
+<header>
+    <div class="nav-bar">
+    
+        <nav class="navbar navbar-expand-lg navbar-light ">
 
-<section id="nav-bar">
-    <nav class="navbar navbar-expand-lg navbar-light ">
-
-        <h2 class="logotext" alt="logotext">AEE</h2>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <i class="fa fa-bars"></i> 
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ml-auto">
-                
-                <li class="nav item">
-                    <div class="input-group">
-                        <input type="text" class="form-control" placeholder="Search..." />
-                        <span class="input-group-btn">
-                    <button class="btn btn-default"><span class="glyphicon glyphicon-search"></span></button>
-                        </span>
-                    </div>
-                </li>
-                <li class="nav item">
-                <div class="dropdown">
-                     <button class="btn btn-default dropdown-toggle" type="button" id="menu1" data-toggle="dropdown">
-                        <a href="#">
-                        <span class="glyphicon glyphicon-globe">
-                    </button>
-                    <ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
-                        <option value="English" >
-                             English 
-                        </option>
-                        <option value="Francais"> 
-                            Francais 
-                        </option>
-                        <option value="Allemand"> 
-                            Arabe 
-                        </option>
-                    </ul>
-                    </span>
-                 </a>
-                 </div>
-                </li>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav item">
+                        <h2 class="logotext" alt="logotext">AEE</h2>
+                    </li>
+                    <li class="nav item">
+                        <div class="search-box">
+                            <input class="search-txt" type="text" name="" placeholder=" type to search   .  .  .">
+                            <a class="search-btn" href="#">  <i class="fa fa-search"></i></a>
+                        </div>
+
+                    </li>
+
+                    <li class="nav item">
+                        <div class="dropdown">
+                            <button class="btn btn-default dropdown-toggle" type="button" id="menu1" data-toggle="dropdown">
+                                <a href="#">
+                                    <span class="glyphicon glyphicon-globe"></span>
+                                </a>
+                            </button>
+                            
+                            <div class="dropdown-menu" role="menu" aria-labelledby="menu1">
+
+                                <ul>
+                                    <li value="English" role="presentation">
+                                        <a role="menuitem" tabindex="-1" href="assets/.php/english.php">
+                                            English 
+                                        </a>
+                                    </li>
+
+                                    <li value="Francais" role="presentation">
+                                        <a role="menuitem" tabindex="-1" href="assets/.php/francais.php">
+                                            Francais 
+                                        </a>
+                                    </li>
+
+                                    <li value="arabe" role="presentation">
+                                        <a role="menuitem" tabindex="-1" href="assets/.php/arabe.php">
+                                            Arabe 
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+       
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#FFF" fill-opacity="1" d="M0,192L30,213.3C60,235,120,277,180,288C240,299,300,277,360,240C420,203,480,149,540,122.7C600,96,660,96,720,117.3C780,139,840,181,900,208C960,235,1020,245,1080,224C1140,203,1200,149,1260,133.3C1320,117,1380,139,1410,149.3L1440,160L1440,0L1410,0C1380,0,1320,0,1260,0C1200,0,1140,0,1080,0C1020,0,960,0,900,0C840,0,780,0,720,0C660,0,600,0,540,0C480,0,420,0,360,0C300,0,240,0,180,0C120,0,60,0,30,0L0,0Z"></path></svg>
+        </nav>
+    
 
 
+    </div>
+        
 
-               
-
-            </ul>
-        </div>
-    </nav>
-</section>
-
+</header>
