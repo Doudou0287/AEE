631f4c815260bd36a08d41af28fc24d1aa0b60c4
AL-KEBSI SAWSAN <salkebsi.etu@unistra.fr>
2020-03-10 00:09:26 +0100

    qq modif

--- assets/.php/ancien_events.php
+++ assets/.php/ancien_events.php
@@ -1,8 +1,17 @@
 <div class="page-wrapper" style="margin-top: 100px;">
-    <div class="row justify-content-center">
-        <div class="col-md-6 col-md-offset-3 text-center">
-            <h1>Anciens evenments</h1>
-        </div>
+    <div class="row">
+            <h1 id="titlebine" class="text-center">Anciens evenments</h1>
+            <img src="assets/img/e1.jpg" alt="ev1" height="202" width="215">
+            <img src="assets/img/e2.jpg" alt="ev2" height="202" width="225">
+            <img src="assets/img/e3.jpg" alt="ev3" height="202" width="215">
+            <img src="assets/img/e4.jpg" alt="ev4" height="202" width="225">
+            <img src="assets/img/e5.jpg" alt="ev5" height="202" width="210">
+            <img src="assets/img/e6.jpg" alt="ev6" height="202" width="210">
+            <img src="assets/img/e7.jpg" alt="ev7" height="202" width="210">
+
+
     </div>
 </div>
-
+<!--
+<div class="col-md-6 col-md-offset-3 text-center">
+            <h1>Anciens evenments</h1>-->
