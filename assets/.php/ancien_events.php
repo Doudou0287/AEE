079795f8e6a9fedaa460efae734e74920abb3b00
s.alkebsi <s.alkebsi@>
2020-03-09 15:56:52 +0100

    add ancien events

---
+++ assets/.php/ancien_events.php
@@ -0,0 +1,8 @@
+<div class="page-wrapper" style="margin-top: 100px;">
+    <div class="row justify-content-center">
+        <div class="col-md-6 col-md-offset-3 text-center">
+            <h1>Anciens evenments</h1>
+        </div>
+    </div>
+</div>
+
--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 require_once("templates/head.php");
 require_once("templates/header.php");
 require_once("templates/sidebar.php");
+require_once("assets/.php/ancien_events.php");
 require_once("templates/footer.php");
 
 
