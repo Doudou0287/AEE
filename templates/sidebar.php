cdab1e15dd900a74361de8531593419e4122a584
s.alkebsi <s.alkebsi@>
2020-04-24 03:14:14 +0200

    sidebar.php

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -1,24 +1,46 @@
+<link rel="stylesheet" href="http://localhost/programmation-web-2---s4---2020-master/assets/.css/sidebar.css">
 
 <div id="sidebar">
 
-    <div class="toggle-btn" onclick="toggleSidebar()">
-        <span></span>
-        <span></span>
-        <span></span>
-    </div>
-    <div class="container-fluid tmargin">
-        <div class="input-group">
-            <input type="text" class="form-control" placeholder="Search..." />
-            <span class="input-group-btn">
-        <button class="btn btn-default"><span class="glyphicon glyphicon-search"></span></button>
-            </span>
+<div class="toggle-btn" onclick="toggleSidebar()">
+    <span></span>
+    <span></span>
+    <span></span>
+</div>
+
+<ul>
+    <li>
+        <div class="container-fluid tmargin">
+            <div class="input-group">
+                <input type="text" class="form-control" placeholder="Search..." />
+                <span class="input-group-btn">
+                    <button class="btn btn-default">
+                        <span class="glyphicon glyphicon-search"></span>
+                </button>
+                </span>
+            </div>
         </div>
-    </div>
-    <ul>
-        <li><a href="http://localhost/programmation-web-2---s4---2020-master/assets/.php/association.php"><span class="glyphicon glyphicon-education">&nbsp;</span>introduction de l’association.</a></li>
-        <li><a href="http://localhost/programmation-web-2---s4---2020-master/assets/.php/evenement.php"><span class="glyphicon glyphicon-calendar">&nbsp;</span> évènement : réservation.</a></li>
-        <li><a href="http://localhost/programmation-web-2---s4---2020-master/assets/.php/commerce.php"><span class="glyphicon glyphicon-shopping-cart">&nbsp;</span> e-commerce : vente.</a></li>
-        <li><a href="http://localhost/programmation-web-2---s4---2020-master/templates/formulaire.php"><span class="glyphicon glyphicon-comment">&nbsp;</span>formulaire</a></li>
-        <!-- li class="side-bar"><a href="assets/.php/evenement.php"><span class="glyphicon glyphicon-calendar">&nbsp;</span> évènement : réservation.</a></li-->
-    </ul>
+    </li>
+    <li style="padding: 15px 30px;">
+    <button  onclick="location.href='http://localhost/programmation-web-2---s4---2020-master/'">
+        <span class="glyphicon glyphicon-education">&nbsp;</span>introduction de lassociation.
+    </button></li>
+    <li style="padding: 15px 30px;">
+    <button  onclick="location.href='http://localhost/programmation-web-2---s4---2020-master/assets/.php/evenement.php'">
+        <span class="glyphicon glyphicon-calendar">&nbsp;</span> évènement : réservation.
+    </button></li>
+    <li style="padding: 15px 30px;">
+    <button  onclick="location.href='http://localhost/programmation-web-2---s4---2020-master/assets/.php/commerce.php'">
+    <span class="glyphicon glyphicon-shopping-cart">&nbsp;</span> e-commerce : vente.
+    </button></li>
+    <li style="padding: 15px 30px;">
+    <button  onclick="location.href='http://localhost/programmation-web-2---s4---2020-master/assets/.php/register.php'">
+    <span class="glyphicon glyphicon-comment">&nbsp;</span>Inscription
+    </button></li>
+    <li style="padding: 15px 30px;">
+    <button  onclick="location.href='http://localhost/programmation-web-2---s4---2020-master/assets/.php/formulaire.php'">    
+        <span class="glyphicon glyphicon-envelope">&nbsp;</span>Deposer announces ici.
+    </button>
+    </li>
+</ul>
 </div>
