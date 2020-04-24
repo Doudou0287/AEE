b1ea85ad2ffc28b085bc6be0346e143914b156e5
s.alkebsi <s.alkebsi@>
2020-04-24 03:27:22 +0200

    page d'acueil

--- assets/.php/ancien_events.php
+++ assets/.php/ancien_events.php
@@ -1,43 +1,53 @@
-<link rel="stylesheet" href="http://localhost/programmation-web-2---s4---2020-master/assets/.css/events.css">
+<link rel="stylesheet" href="http://localhost/programmation-web-2---s4---2020-master/assets/.css/event.css">
 
 
 
-<h1 id="titlebine" class="text-center"></h1>
+<div class="page">
 
-<div class="containe">
+    <h1 style="font-weight: 700px;">AEE "Association étudiants étrangers"</h1>
+    <p>
+        Une Association événementielle qui conçoit et organise divers événements pour les étudiants étrangers .
+qui aide les étudiants étrangers à s'adapter à leur situation. Aidez-les à trouver des evenement, des commerces, des soldes ...<a href="#">read more</a>
+    </p>
+            
+          
+    
+
+
+    <div class="containe1">
         <div class="mySlides">
             <div class="numbertext">1 / 6</div>
-            <img src="http://localhost/programmation-web-2---s4---2020-master/assets/img/e4.jpg" style="width:100%">
+            <img src="http://localhost/programmation-web-2---s4---2020-master/assets/img/a4.jpg" height="540" width="100%">
 
         </div>
 
         <div class="mySlides">
             <div class="numbertext">2 / 6</div>
-            <img src="http://localhost/programmation-web-2---s4---2020-master/assets/img/e2.jpg" style="width:100%">
+            <img src="http://localhost/programmation-web-2---s4---2020-master/assets/img/e2.jpg" height="540" width="100%">
         </div>
 
         <div class="mySlides">
             <div class="numbertext">3 / 6</div>
-            <img src="http://localhost/programmation-web-2---s4---2020-master/assets/img/e3.jpg" style="width:100%">
+            <img src="http://localhost/programmation-web-2---s4---2020-master/assets/img/e3.jpg" height="540" width="100%">
         </div>
 
         <div class="mySlides">
             <div class="numbertext">4 / 6</div>
-            <img src="http://localhost/programmation-web-2---s4---2020-master/assets/img/e1.jpg" style="width:100%">
+            <img src="http://localhost/programmation-web-2---s4---2020-master/assets/img/e1.jpg" height="540" width="100%">
         </div>
 
         <div class="mySlides">
             <div class="numbertext">5 / 6</div>
-            <img src="http://localhost/programmation-web-2---s4---2020-master/assets/img/e5.jpg" style="width:100%">
+            <img src="http://localhost/programmation-web-2---s4---2020-master/assets/img/a5.jpg" height="540" width="100%">
         </div>
 
         <div class="mySlides">
             <div class="numbertext">6 / 6</div>
-            <img src="http://localhost/programmation-web-2---s4---2020-master/assets/img/e6.jpg" style="width:100%">
+            <img src="http://localhost/programmation-web-2---s4---2020-master/assets/img/e6.jpg" height="540" width="100%">
         </div>
 
-        <a class="prev" onclick="plusSlides(-1)">❮</a>
-        <a class="next" onclick="plusSlides(1)">❯</a>
+        <a class="prev" onclick="plusSlides(-1)"></a>
+        <a class="next" onclick="plusSlides(1)"></a>
 
         <div class="caption-container">
             <p id="caption"></p>
@@ -45,59 +55,28 @@
 
         <div class="row">
             <div class="column">
-                <img class="demo cursor" src="http://localhost/programmation-web-2---s4---2020-master/assets/img/e4.jpg" height="150" width="100%" onclick="currentSlide(1)" alt="The Woods">
+                <img class="demo cursor" src="http://localhost/programmation-web-2---s4---2020-master/assets/img/a4.jpg" height="150" width="100%" onclick="currentSlide(1)" alt="Galerie">
             </div>
             <div class="column">
-                <img class="demo cursor" src="http://localhost/programmation-web-2---s4---2020-master/assets/img/e2.jpg" height="150" width="100%" onclick="currentSlide(2)" alt="Cinque Terre">
+                <img class="demo cursor" src="http://localhost/programmation-web-2---s4---2020-master/assets/img/e2.jpg" height="150" width="100%" onclick="currentSlide(2)" alt="Temps de PeintureS">
             </div>
             <div class="column">
-                <img class="demo cursor" src="http://localhost/programmation-web-2---s4---2020-master/assets/img/e3.jpg" height="150" width="100%" onclick="currentSlide(3)" alt="Mountains and fjords">
+                <img class="demo cursor" src="http://localhost/programmation-web-2---s4---2020-master/assets/img/e3.jpg" height="150" width="100%" onclick="currentSlide(3)" alt="Bataille de chant">
             </div>
             <div class="column">
-                <img class="demo cursor" src="http://localhost/programmation-web-2---s4---2020-master/assets/img/e1.jpg" height="150" width="100%" onclick="currentSlide(4)" alt="Northern Lights">
+                <img class="demo cursor" src="http://localhost/programmation-web-2---s4---2020-master/assets/img/e1.jpg" height="150" width="100%" onclick="currentSlide(4)" alt="C'est L'heure du Sport">
             </div>
             <div class="column">
-                <img class="demo cursor" src="http://localhost/programmation-web-2---s4---2020-master/assets/img/e5.jpg" height="150" width="100%" onclick="currentSlide(5)" alt="Nature and sunrise">
+                <img class="demo cursor" src="http://localhost/programmation-web-2---s4---2020-master/assets/img/a5.jpg" height="150" width="100%" onclick="currentSlide(5)" alt="Seance Musical">
             </div>
             <div class="column">
-                <img class="demo cursor" src="http://localhost/programmation-web-2---s4---2020-master/assets/img/e6.jpg" height="150" width="100%" onclick="currentSlide(6)" alt="Snowy Mountains">
+                <img class="demo cursor" src="http://localhost/programmation-web-2---s4---2020-master/assets/img/e6.jpg" height="150" width="94.5%" onclick="currentSlide(6)" alt="Concert">
             </div>
         </div>
     </div>
 
-    <h2 style="text-align:center">Anciens evenments</h2>
+    <h2>Anciens evenments</h2>
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-<!--
-
-<div class="page-wrapper" style="margin-top: 100px;">
-    <div class="row">
-            <h1 id="titlebine" class="text-center"></h1>
-            <img src="http://localhost/programmation-web-2---s4---2020-master/assets/img/e1.jpg" alt="ev1" height="202" width="215">
-            <img src="http://localhost/programmation-web-2---s4---2020-master/assets/img/e2.jpg" alt="ev2" height="202" width="225">
-            <img src="http://localhost/programmation-web-2---s4---2020-master/assets/img/e3.jpg" alt="ev3" height="202" width="215">
-            <img src="http://localhost/programmation-web-2---s4---2020-master/assets/img/e4.jpg" alt="ev4" height="202" width="225">
-            <img src="http://localhost/programmation-web-2---s4---2020-master/assets/img/e5.jpg" alt="ev5" height="202" width="210">
-            <img src="http://localhost/programmation-web-2---s4---2020-master/assets/img/e6.jpg" alt="ev6" height="202" width="210">
-            <img src="http://localhost/programmation-web-2---s4---2020-master/assets/img/e7.jpg" alt="ev7" height="202" width="210">
-    </div>
-    <div class="col-md-6 col-md-offset-3 text-center">
-        <h2>Anciens evenments</h2> 
-    </div>
 </div>
 
-<div class="col-md-6 col-md-offset-3 text-center">
-            <h1>Anciens evenments</h1>-->
