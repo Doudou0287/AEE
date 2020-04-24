51e75df2be838aff13402cad0cd789196f942bb1
s.alkebsi <s.alkebsi@>
2020-04-24 03:43:40 +0200

    evenement et traiter evenement

--- assets/.php/evenement.php
+++ assets/.php/evenement.php
@@ -1,35 +1,63 @@
-
 <?php
-include '../../templates/head.php';
+
+$con = new PDO('mysql:host=localhost;dbname=aee','root','');
+
+$pdoStat = $con->prepare('SELECT * FROM announce1 ORDER BY DATE_MESSAGE DESC');
+
+$execute = $pdoStat->execute();
+
+$commerces = $pdoStat->fetchAll();
+
 include '../../templates/header.php';
 include '../../templates/sidebar.php';
 ?>
-<div class="announce">
-<ul>
+   <link rel="stylesheet" href="http://localhost/programmation-web-2---s4---2020-master/assets/.css/form.css">
 
-    <li>
-        La Semaine de l'EMPLOI Ostwald-Illkirch
-    </li>
-    <a href="https://facebook.com/events/s/la-semaine-de-lemploi-ostwald-/512391319713513/ ">savoir plus</a>
-    <li>
-        La Semaine de l'EMPLOI Ostwald-Illkirch
-    </li>
-    <a href="https://facebook.com/events/s/la-semaine-de-lemploi-ostwald-/512391319713513/ ">savoir plus</a>
-    <li>
-        La Semaine de l'EMPLOI Ostwald-Illkirch
-    </li>
-    <a href="https://facebook.com/events/s/la-semaine-de-lemploi-ostwald-/512391319713513/ ">savoir plus</a>
-    <li>
-        La Semaine de l'EMPLOI Ostwald-Illkirch
-    </li>
-    <a href="https://facebook.com/events/s/la-semaine-de-lemploi-ostwald-/512391319713513/ ">savoir plus</a>
-    <li>
-        La Semaine de l'EMPLOI Ostwald-Illkirch
-    </li>
-    <a href="https://facebook.com/events/s/la-semaine-de-lemploi-ostwald-/512391319713513/ ">savoir plus</a>
-</ul>
-</div>
 
+   <section>
+	<h3 class="try">LISTE DES ANNOUNCES DES EVENEMENT</h3>
+    <table>
+        
+        <thead>
+            <tr>
+                <th scope="col"><span>DATE</span></th>
+                <th scope="col"><span >NOM</span></th>
+                <th scope="col"><span >PRENOM</span></th>
+                <th scope="col"><span >SUJET</span></th>
+                <th scope="col"><span >ANNOUNCE</span></th>
+                <th scope="col"><span >LINKS</span></th>
+
+            </tr>
+        </thead>
+        <?php foreach ($commerces as $announce): ?>
+	    <tbody>
+
+            <tr>
+                <th  scope="row"><?= $announce['DATE_MESSAGE']?></th>
+                <td><?= $announce['NOM']?></td>
+                <td><?= $announce['PRENOM']?></td>
+                <td><?= $announce['SUJET']?></td>
+                <td><?= $announce['MESSAGE']?></td>
+                <td class="text"><a href="<?= $announce['LINKS']?>">Savoir plus</a></td>
+            </tr>
+
+        </tbody>
+        <?php endforeach; ?>
+    </table>	
+
+</section>
+
+
+
+
+<center>
+    <button style="background: hsla(325, 10%, 47%, 0.342);" onclick="location.href='http://localhost/programmation-web-2---s4---2020-master/assets/.php/register.php'" type="button">
+        <h2 >deposer une announce d'evenement ici</h2>
+        </button> 
+    </center> 
+                
+         
 <?php
 include '../../templates/footer.php';
 ?>
+
---
+++ assets/.php/traitement-evenement.php
@@ -0,0 +1,34 @@
+('mysql:host=localhost;dbname=aee','root','');
+
+	$pdoStat = $con->prepare('INSERT INTO announce1 VALUE (NULL, :nom, :prenom, :sujet, :msg, :date_msg, :links)');
+	
+	$pdoStat->bindValue(':nom', $_POST['NOM'], PDO::PARAM_STR);
+	$pdoStat->bindValue(':prenom', $_POST['PRENOM'], PDO::PARAM_STR);
+	
+	$pdoStat->bindValue(':sujet', $_POST['SUJET'], PDO::PARAM_STR);
+	$pdoStat->bindValue(':msg', $_POST['MESSAGE'], PDO::PARAM_STR);
+	$pdoStat->bindValue(':date_msg', date("Y-m-d H:i:s"), PDO::PARAM_STR);
+	$pdoStat->bindValue(':links', $_POST['Links'], PDO::PARAM_STR);
+
+
+	$insert = $pdoStat->execute();
+
+	if ($insert) {
+		$message = 'Votre announce a été déposé';
+	}
+	
+	else{
+		$message = 'Echec ...';
+	}
+	
+?>
+
+
+<center>
+<h1 style="color: rebeccapurple"><?php echo $message; ?> </h1>
+</center>
+
+<?php 
+	header("refresh:2; url=http://localhost/programmation-web-2---s4---2020-master/assets/.php/evenement.php");
+	?>
+
